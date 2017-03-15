0653e34245c84859036adce019f8bb447f0cc161
Jason Whittle <jason.whittle@gmail.com>
2017-03-15 18:42:02 -0400

    Update stack to LTS-8.5 and change validator interface.

--- src/Yesod/Auth/Http/BearerToken.hs
+++ src/Yesod/Auth/Http/BearerToken.hs
@@ -5,11 +5,11 @@
 module Yesod.Auth.Http.BearerToken
        ( defaultMaybeBearerTokenAuthId
        , maybeBearerTokenAuthId
-       , TokenValidator(..)
        ) where
 
 import           Control.Monad.Catch (MonadThrow)
 import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.Time (MonadTime)
 import           Control.Monad.Trans.Control (MonadBaseControl)
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
@@ -26,19 +26,13 @@ newtype CachedBearerTokenAuthId a = CachedBearerTokenAuthId { unCached :: Maybe 
                                   deriving Typeable
 
 
--- | A pair of functions to validate user bearer token
-data TokenValidator m = TokenValidator
-                      { validateToken :: ByteString -> m Bool
-                      , extractSubject :: ByteString -> Maybe Text
-                      }
-
-
 -- | Retrieve the 'AuthId' using Authorization header.
 --
 -- If valid credentials are found and authorized the auth id is
 -- cached.
-defaultMaybeBearerTokenAuthId :: (MonadIO m, MonadThrow m, MonadBaseControl IO m)
-                              => TokenValidator (HandlerT site m) -> HandlerT site m (Maybe Text)
+defaultMaybeBearerTokenAuthId :: (Monad m, MonadBaseControl IO m, MonadIO m, MonadThrow m, MonadTime m)
+                              => (ByteString -> HandlerT site m (Maybe Text))
+                              -> HandlerT site m (Maybe Text)
 defaultMaybeBearerTokenAuthId auth =
   cachedAuth $ waiRequest >>= maybeBearerTokenAuthId auth
 
@@ -51,19 +45,17 @@ cachedAuth = fmap unCached . cached . fmap CachedBearerTokenAuthId
 
 -- | Use the bearer token in the HTTP _Authorization_ header to
 -- retrieve the AuthId of request
-maybeBearerTokenAuthId :: MonadIO m => TokenValidator (HandlerT site m) -> Request -> HandlerT site m (Maybe Text)
-maybeBearerTokenAuthId TokenValidator{..} req =
+maybeBearerTokenAuthId :: (Monad m, MonadTime m)
+                       => (ByteString -> HandlerT site m (Maybe Text))
+                       -> Request
+                       -> HandlerT site m (Maybe Text)
+maybeBearerTokenAuthId f req =
   case authorization of
    Just (strategy, token)
      | BS.map toLower strategy == "bearer" ->
-         authorizeCredentials $ BS.dropWhile isSpace token
+         f $ BS.dropWhile isSpace token
      | otherwise -> return Nothing
    _ -> return Nothing
   where
     authorization = BS.break isSpace
                 <$> lookup "Authorization" (requestHeaders req)
-    authorizeCredentials token = do
-      authorized <- validateToken token
-      return $ if authorized
-               then extractSubject token
-               else Nothing
